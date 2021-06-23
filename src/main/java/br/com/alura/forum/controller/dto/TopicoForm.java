3c3bc9cf92dd7d390aa8585b5841e36bcc1fdc34
gtanques <saintcalm1@gmail.com>
2021-06-23 01:17:47 -0300

    entidades, dtos, tratamento de erros e crud topico

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoForm.java
@@ -0,0 +1,52 @@
+package br.com.alura.forum.controller.dto;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class TopicoForm {
+    @NotNull @Length(min = 5)
+    private String titulo;
+
+    @NotNull @Length(min = 5)
+    private String mensagem;
+
+    @NotNull @NotEmpty
+    private String nomeCurso;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public TopicoForm() {
+    }
+
+    public Topico converter(CursoRepository repository) {
+        Curso curso = repository.findByNome(nomeCurso);
+        return new Topico(titulo, mensagem, curso);
+    }
+}
