b4240d19c7f4121bd64e1725f6ec025112e9519d
gtanques <saintcalm1@gmail.com>
2021-06-24 16:04:26 -0300

    criada autenticação via token

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$PiRH74tbY8d6nMRCvB8lBOJJGT9AaAEPbBUacZEzbKaixNYWKqnfu');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
